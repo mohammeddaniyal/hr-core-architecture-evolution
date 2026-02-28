8aefeffa03b37f82462dd9c639391aea24f040dc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2026-02-28 14:18:25 +0530

    refactor(stage3): externalize database credentials using environment variables

--- stage-3-ajax-json/src/io/github/mohammeddaniyal/hr/dl/DAOConnection.java
+++ stage-3-ajax-json/src/io/github/mohammeddaniyal/hr/dl/DAOConnection.java
@@ -9,10 +9,20 @@ Connection connection=null;
 try
 {
 Class.forName("com.mysql.cj.jdbc.Driver");
-connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/tmdb","tmdbuser","tmdbuser");
+
+String url= System.getenv("DB_URL");
+String username=System.getenv("DB_USERNAME");
+String password=System.getenv("DB_PASSWORD");
+if(url==null || username==null || password==null)
+{
+    throw new DAOException("Database environment variables not set.");
+}
+connection=DriverManager.getConnection(url, username, password);
+
 return connection;
 }catch(Exception exception)
 {
+    exception.printStackTrace();
 throw new DAOException(exception.getMessage());
 }
 }
